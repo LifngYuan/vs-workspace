4684af3105cba3db0d856c26a48d342eac616120
袁利锋 <lf.yuan@outlook.com>
2022-01-03 15:30:34 +0800

    添加例子3

---
+++ PythonProject/example3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   example3.py
+@Time    :   2022/01/03 15:09:38
+@Author  :   袁利锋
+@Contact :   lifng.yuan@gmail.com
+@Department   :  None
+@Desc    :   None
+'''
+
+# here put the import lib
+
+# 将两个数按照从小到大排序
