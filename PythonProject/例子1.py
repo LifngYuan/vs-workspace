3f13776d72ef061d31779e47052e7d04a70ff3be
lifng.yuan <lf.yuan@outlook.com>
2022-01-03 15:02:46 +0800

    新建例子1.py
    举例说明   计算一个数的绝对值

---
+++ PythonProject/例子1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   例子1.py
+@Time    :   2022/01/03 14:58:17
+@Author  :   袁利锋
+@Contact :   lifng.yuan@gmail.com
+@Department   :  None
+@Desc    :   None
+'''
+
+# here put the import lib
+x=-4
+if x<=0:
+    x=-x
+print(x)
+
+# 计算两个数中的较大值
