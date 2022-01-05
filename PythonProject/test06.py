3224d8b38ba32291e41cf38987ad05c70ed14348
lifng.yuan <lf.yuan@outlook.com>
2022-01-05 13:49:22 +0800

    if表达式

---
+++ PythonProject/test06.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   test06.py
+@Time    :   2022/01/05 13:31:12
+@Author  :   袁利锋
+@Contact :   lifng.yuan@gmail.com
+@Department   :  None
+@Desc    :   None
+'''
+
+# here put the import lib
+# 计算最大值
+'''
+x=999
+y=66
+z=x if x>y else y
+print(z)
+'''
+
+# 计算绝对值
+# x=-9
+# print(x if x>0 else -x)
+
+# shiyong if biaodashi ,panduanyigeshu de jiouxing
+
+num =int(input("input a number:"))
+print(num,"是奇数" if num%2==1 else "是偶数")
